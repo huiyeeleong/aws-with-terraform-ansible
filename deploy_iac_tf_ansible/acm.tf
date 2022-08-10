1d25d24acecf8b21acd73058f5eeb0dce039f5d5
Leong <hui.yee.leong@accenture.com>
2022-08-10 21:50:13 +1000

    Updated the output

--- deploy_iac_tf_ansible/acm.tf
+++ deploy_iac_tf_ansible/acm.tf
@@ -19,18 +19,5 @@ resource "aws_acm_certificate_validation" "cert" {
   validation_record_fqdns = [aws_route53_record.cert_validation[each.key].fqdn]
 }
 
-####ACM CONFIG END
 
-resource "aws_lb_listener" "jenkins-listener" {
-  provider          = aws.region-master
-  load_balancer_arn = aws_lb.application-lb.arn
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate.jenkins-lb-https.arn
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app-lb-tg.arn
-  }
-}
 
--- deploy_iac_tf_ansible/variables.tf
+++ deploy_iac_tf_ansible/variables.tf
@@ -30,17 +30,6 @@ variable "instance-type" {
   default = "t3.micro"
 }
 
-###This chunk of template can also be put inside outputs.tf for better segregation 
-output "Jenkins-Main-Node-Public-IP" {
-  value = aws_instance.jenkins-master.public_ip
-}
-
-output "Jenkins-Worker-Public-IPs" {
-  value = {
-    for instance in aws_instance.jenkins-worker-oregon :
-    instance.id => instance.public_ip
-  }
-}
 
 #Add the variable webserver-port to variables.tf
 variable "webserver-port" {
